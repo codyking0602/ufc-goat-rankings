ff880586cd3d1fa96d9b6ae608e2ed64e9629f01
codyking0602 <bcking06@gmail.com>
2026-07-01 07:53:51 -0500

    Clean compare data entrypoints

---
+++ assets/data/fight-ledger.js
@@ -0,0 +1,7 @@
+(function () {
+  // Central Fight Ledger file.
+  // Existing direct fight/rivalry entries are currently loaded through assets/data/compare-profiles.js
+  // because the legacy compare packs contain both profiles and ledger entries.
+  // New direct fight/rivalry entries should be moved here as the compare data is fully consolidated.
+  window.FIGHT_LEDGER_FILE_READY = true;
+})();
