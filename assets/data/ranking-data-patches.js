81c2124de865fdd618b9da85f99e5a29f1d2ca43
codyking0602 <bcking06@gmail.com>
2026-07-02 23:05:46 -0500

    Load updated Cain Merab BJ Watch Moment packets

--- assets/data/ranking-data-patches.js
+++ assets/data/ranking-data-patches.js
@@ -1,6 +1,6 @@
 // Lightweight post-load status hook.
 (function(){
-  const VERSION = 'ranking-data-patches-20260702au-women-ledger-packets';
+  const VERSION = 'ranking-data-patches-20260702av-cain-merab-bj-watch-moments';
   const SLUG_OVERRIDES = {
     'B.J. Penn':'bj-penn','BJ Penn':'bj-penn','Georges St-Pierre':'georges-st-pierre','T.J. Dillashaw':'tj-dillashaw','TJ Dillashaw':'tj-dillashaw','Junior dos Santos':'junior-dos-santos','Mauricio Rua':'mauricio-rua','Maurício Rua':'mauricio-rua','Zabit Magomedsharipov':'zabit-magomedsharipov'
   };
@@ -82,9 +82,9 @@
       {src:'assets/data/fighter-packets/ilia-topuria.js?v=fighter-packet-ilia-topuria-20260702a',attr:'data-fighter-packet-ilia-topuria'},
       {src:'assets/data/fighter-packets/israel-adesanya.js?v=fighter-packet-israel-adesanya-20260702a',attr:'data-fighter-packet-israel-adesanya'},
       {src:'assets/data/fighter-packets/petr-yan.js?v=fighter-packet-petr-yan-20260702b',attr:'data-fighter-packet-petr-yan'},
-      {src:'assets/data/fighter-packets/cain-velasquez.js?v=fighter-packet-cain-velasquez-20260702a',attr:'data-fighter-packet-cain-velasquez'},
-      {src:'assets/data/fighter-packets/merab-dvalishvili.js?v=fighter-packet-merab-dvalishvili-20260702a',attr:'data-fighter-packet-merab-dvalishvili'},
-      {src:'assets/data/fighter-packets/bj-penn.js?v=fighter-packet-bj-penn-20260702a',attr:'data-fighter-packet-bj-penn'},
+      {src:'assets/data/fighter-packets/cain-velasquez.js?v=fighter-packet-cain-velasquez-20260702b',attr:'data-fighter-packet-cain-velasquez'},
+      {src:'assets/data/fighter-packets/merab-dvalishvili.js?v=fighter-packet-merab-dvalishvili-20260702b',attr:'data-fighter-packet-merab-dvalishvili'},
+      {src:'assets/data/fighter-packets/bj-penn.js?v=fighter-packet-bj-penn-20260702b',attr:'data-fighter-packet-bj-penn'},
       {src:'assets/data/fighter-packets/alex-pereira.js?v=fighter-packet-alex-pereira-20260702a',attr:'data-fighter-packet-alex-pereira'},
       {src:'assets/data/fighter-packets/chuck-liddell.js?v=fighter-packet-chuck-liddell-20260702a',attr:'data-fighter-packet-chuck-liddell'},
       {src:'assets/data/fighter-packets/dominick-cruz.js?v=fighter-packet-dominick-cruz-20260702a',attr:'data-fighter-packet-dominick-cruz'},
