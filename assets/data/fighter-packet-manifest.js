1bafc68a11cd1a9e5260bb28c7983117ab3c7676
codyking0602 <bcking06@gmail.com>
2026-07-06 09:53:42 -0500

    Register Lyoto Machida fighter packet

--- assets/data/fighter-packet-manifest.js
+++ assets/data/fighter-packet-manifest.js
@@ -1,7 +1,7 @@
 // Central manifest for fighter packet modules.
 // Add new fighter packet files here instead of growing ranking-data-patches.js.
 (function(){
-  const VERSION='fighter-packet-manifest-20260706e-tyron-woodley';
+  const VERSION='fighter-packet-manifest-20260706f-lyoto-machida';
   const packets=[
     {slug:'demetrious-johnson',version:'20260702a'},
     {slug:'anderson-silva',version:'20260702a'},
@@ -35,6 +35,7 @@
     {slug:'conor-mcgregor',version:'20260702a'},
     {slug:'justin-gaethje',version:'20260702d'},
     {slug:'frankie-edgar',version:'20260703b'},
+    {slug:'lyoto-machida',version:'20260706a'},
     {slug:'sean-strickland',version:'20260706a'},
     {slug:'robert-whittaker',version:'20260706b-round-control'},
     {slug:'sean-omalley',version:'20260706b-round-control'},
