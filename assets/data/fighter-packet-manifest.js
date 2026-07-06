5ff769620a542489228902e05442b7f411398c3b
codyking0602 <bcking06@gmail.com>
2026-07-06 09:10:27 -0500

    Add Tyron Woodley to packet manifest

--- assets/data/fighter-packet-manifest.js
+++ assets/data/fighter-packet-manifest.js
@@ -1,7 +1,7 @@
 // Central manifest for fighter packet modules.
 // Add new fighter packet files here instead of growing ranking-data-patches.js.
 (function(){
-  const VERSION='fighter-packet-manifest-20260706d-sean-strickland';
+  const VERSION='fighter-packet-manifest-20260706e-tyron-woodley';
   const packets=[
     {slug:'demetrious-johnson',version:'20260702a'},
     {slug:'anderson-silva',version:'20260702a'},
@@ -16,6 +16,7 @@
     {slug:'daniel-cormier',version:'20260702a'},
     {slug:'stipe-miocic',version:'20260702a'},
     {slug:'dricus-du-plessis',version:'20260705e-prime-195'},
+    {slug:'tyron-woodley',version:'20260706a'},
     {slug:'ilia-topuria',version:'20260705a'},
     {slug:'israel-adesanya',version:'20260702a'},
     {slug:'aljamain-sterling',version:'20260703a'},
