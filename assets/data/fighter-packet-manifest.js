a12c384e26670f460417c0baf16020c3af8ebf76
codyking0602 <bcking06@gmail.com>
2026-07-06 07:12:26 -0500

    Update Dricus packet manifest for Apex adjustment

--- assets/data/fighter-packet-manifest.js
+++ assets/data/fighter-packet-manifest.js
@@ -1,7 +1,7 @@
 // Central manifest for fighter packet modules.
 // Add new fighter packet files here instead of growing ranking-data-patches.js.
 (function(){
-  const VERSION='fighter-packet-manifest-20260705d-dricus-round-control';
+  const VERSION='fighter-packet-manifest-20260705e-dricus-apex-adjust';
   const packets=[
     {slug:'demetrious-johnson',version:'20260702a'},
     {slug:'anderson-silva',version:'20260702a'},
@@ -15,7 +15,7 @@
     {slug:'matt-hughes',version:'20260702a'},
     {slug:'daniel-cormier',version:'20260702a'},
     {slug:'stipe-miocic',version:'20260702a'},
-    {slug:'dricus-du-plessis',version:'20260705c-round-control'},
+    {slug:'dricus-du-plessis',version:'20260705d-apex-adjust'},
     {slug:'ilia-topuria',version:'20260705a'},
     {slug:'israel-adesanya',version:'20260702a'},
     {slug:'aljamain-sterling',version:'20260703a'},
