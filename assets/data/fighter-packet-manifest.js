2f84f11fda5d3dd78c970f9970b0fb86ee367472
codyking0602 <bcking06@gmail.com>
2026-07-05 22:36:05 -0500

    Add fighter packet manifest

---
+++ assets/data/fighter-packet-manifest.js
@@ -0,0 +1,50 @@
+// Central manifest for fighter packet modules.
+// Add new fighter packet files here instead of growing ranking-data-patches.js.
+(function(){
+  const VERSION='fighter-packet-manifest-20260705a';
+  const packets=[
+    {slug:'demetrious-johnson',version:'20260702a'},
+    {slug:'anderson-silva',version:'20260702a'},
+    {slug:'khabib-nurmagomedov',version:'20260702a'},
+    {slug:'islam-makhachev',version:'20260702a'},
+    {slug:'alexander-volkanovski',version:'20260703b'},
+    {slug:'randy-couture',version:'20260702a'},
+    {slug:'max-holloway',version:'20260703b'},
+    {slug:'kamaru-usman',version:'20260702a'},
+    {slug:'jose-aldo',version:'20260705b'},
+    {slug:'matt-hughes',version:'20260702a'},
+    {slug:'daniel-cormier',version:'20260702a'},
+    {slug:'stipe-miocic',version:'20260702a'},
+    {slug:'ilia-topuria',version:'20260705a'},
+    {slug:'israel-adesanya',version:'20260702a'},
+    {slug:'aljamain-sterling',version:'20260703a'},
+    {slug:'petr-yan',version:'20260702b'},
+    {slug:'cain-velasquez',version:'20260702b'},
+    {slug:'merab-dvalishvili',version:'20260702b'},
+    {slug:'bj-penn',version:'20260702b'},
+    {slug:'dustin-poirier',version:'20260703a'},
+    {slug:'tj-dillashaw',version:'20260703a'},
+    {slug:'alex-pereira',version:'20260702c'},
+    {slug:'chuck-liddell',version:'20260702a'},
+    {slug:'dominick-cruz',version:'20260702a'},
+    {slug:'francis-ngannou',version:'20260702a'},
+    {slug:'charles-oliveira',version:'20260702a'},
+    {slug:'henry-cejudo',version:'20260702a'},
+    {slug:'conor-mcgregor',version:'20260702a'},
+    {slug:'justin-gaethje',version:'20260702d'},
+    {slug:'frankie-edgar',version:'20260703b'},
+    {slug:'dan-henderson',version:'20260703a'},
+    {slug:'amanda-nunes',version:'20260702a'},
+    {slug:'valentina-shevchenko',version:'20260702a'},
+    {slug:'joanna-jedrzejczyk',version:'20260702b'},
+    {slug:'ronda-rousey',version:'20260702b'}
+  ];
+  window.UFC_FIGHTER_PACKET_MANIFEST={
+    version:VERSION,
+    updated:'2026-07-05',
+    purpose:'Central packet list for scalable fighter additions.',
+    count:packets.length,
+    fighters:packets.map(p=>p.slug),
+    packets
+  };
+})();
