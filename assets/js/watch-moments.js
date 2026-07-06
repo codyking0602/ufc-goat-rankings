29dad90059ed9ee5b00503d192dcf89aa7c94597
codyking0602 <bcking06@gmail.com>
2026-07-06 09:54:09 -0500

    Add Lyoto Machida Watch Moment

--- assets/js/watch-moments.js
+++ assets/js/watch-moments.js
@@ -1,7 +1,7 @@
 // Watch Moment links for fighter cards and profiles.
 // Keep links here as app-facing content, separate from scoring.
 (function(){
-  const VERSION = 'watch-moments-20260706d-tyron-woodley';
+  const VERSION = 'watch-moments-20260706e-lyoto-machida';
   if(typeof DISPLAY_OVERRIDES === 'undefined') return;
 
   const WATCH_MOMENTS = {
@@ -39,6 +39,7 @@
     'Conor McGregor': 'https://youtube.com/shorts/eeHdLpBUmlU?is=rKzl28sGEKreaI2g',
     'Justin Gaethje': 'https://youtube.com/shorts/2LxEazU0vuM?is=tHj1Dxylleh4yGG7',
     'Frankie Edgar': 'https://youtube.com/shorts/lLpRwEN3PJk?is=QVVQjKx_0gVmw-wO',
+    'Lyoto Machida': 'https://youtube.com/shorts/egQH6YZhYE8?is=dPiUxTvLAK3X4qu6',
     'Sean Strickland': 'https://youtube.com/shorts/oOeeWkSuOIo?si=f2F9cDqI4ZOvPaKh',
     'Robert Whittaker': 'https://youtube.com/shorts/mmIGDqLaRVM?is=n7F_4C410g2bf3nm',
     "Sean O'Malley": 'https://youtube.com/shorts/Qelywtchvk8?is=C0v8L_ndxdC5BS9c',
