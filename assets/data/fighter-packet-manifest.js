802db8e2437854dba157c4489a6f129c97bb8b23
codyking0602 <bcking06@gmail.com>
2026-07-06 08:09:08 -0500

    Cache bust Sean and Whittaker round-control packets

--- assets/data/fighter-packet-manifest.js
+++ assets/data/fighter-packet-manifest.js
@@ -1,7 +1,7 @@
 // Central manifest for fighter packet modules.
 // Add new fighter packet files here instead of growing ranking-data-patches.js.
 (function(){
-  const VERSION='fighter-packet-manifest-20260706b-robert-whittaker';
+  const VERSION='fighter-packet-manifest-20260706c-round-control';
   const packets=[
     {slug:'demetrious-johnson',version:'20260702a'},
     {slug:'anderson-silva',version:'20260702a'},
@@ -34,8 +34,8 @@
     {slug:'conor-mcgregor',version:'20260702a'},
     {slug:'justin-gaethje',version:'20260702d'},
     {slug:'frankie-edgar',version:'20260703b'},
-    {slug:'robert-whittaker',version:'20260706a'},
-    {slug:'sean-omalley',version:'20260706a'},
+    {slug:'robert-whittaker',version:'20260706b-round-control'},
+    {slug:'sean-omalley',version:'20260706b-round-control'},
     {slug:'dan-henderson',version:'20260703a'},
     {slug:'amanda-nunes',version:'20260702a'},
     {slug:'valentina-shevchenko',version:'20260702a'},
