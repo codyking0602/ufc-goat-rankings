84bb4f3e85420828f71ead64d03f761507326a58
codyking0602 <bcking06@gmail.com>
2026-07-06 12:42:19 -0500

    Register Zhang Weili fighter packet

--- assets/data/fighter-packet-manifest.js
+++ assets/data/fighter-packet-manifest.js
@@ -1,7 +1,7 @@
 // Central manifest for fighter packet modules.
 // Add new fighter packet files here instead of growing ranking-data-patches.js.
 (function(){
-  const VERSION='fighter-packet-manifest-20260706l-junior-dos-santos';
+  const VERSION='fighter-packet-manifest-20260706m-zhang-weili';
   const packets=[
     {slug:'demetrious-johnson',version:'20260702a'},
     {slug:'anderson-silva',version:'20260702a'},
@@ -46,6 +46,7 @@
     {slug:'dan-henderson',version:'20260703a'},
     {slug:'amanda-nunes',version:'20260702a'},
     {slug:'valentina-shevchenko',version:'20260702a'},
+    {slug:'zhang-weili',version:'20260706a'},
     {slug:'joanna-jedrzejczyk',version:'20260702b'},
     {slug:'ronda-rousey',version:'20260702b'}
   ];
