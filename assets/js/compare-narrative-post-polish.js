a043ab879a73f0881c783015a30e6a1fcc5c94e4
codyking0602 <bcking06@gmail.com>
2026-07-07 14:52:38 -0500

    Delete assets/js/compare-narrative-post-polish.js

--- assets/js/compare-narrative-post-polish.js
+++
@@ -1,3 +0,0 @@
-// LEGACY / INACTIVE.
-// Compare post-polish logic has been consolidated into the active compare renderer stack.
-// This file is not loaded by the app and can be deleted manually from GitHub if desired.
