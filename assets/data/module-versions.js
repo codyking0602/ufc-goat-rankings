e7a76a808aa13ce52bbe4c78b987ada046f29fa3
codyking0602 <bcking06@gmail.com>
2026-07-08 14:48:13 -0500

    Bump elite stakes subledger version

--- assets/data/module-versions.js
+++ assets/data/module-versions.js
@@ -2,7 +2,7 @@
 window.UFC_MODULE_VERSIONS = {
   primeWindows: "20260708a",
   primeRoundControlAudit: "20260708a",
-  primeDominanceLedgers: "20260708f-linear-prime-record",
+  primeDominanceLedgers: "20260708g-elite-stakes-subledger",
   scoreWeighting: "20260708d-prime-dominance-data-restart-loader",
   championshipResumeLive: "20260708e",
   opponentQualityLive: "20260708b"
