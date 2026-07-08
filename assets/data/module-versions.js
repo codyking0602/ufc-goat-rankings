1a91dfb1f3656cdf5c1b6c93fd9f13292800a396
codyking0602 <bcking06@gmail.com>
2026-07-08 15:05:16 -0500

    Bump elite stakes backfill version

--- assets/data/module-versions.js
+++ assets/data/module-versions.js
@@ -2,7 +2,7 @@
 window.UFC_MODULE_VERSIONS = {
   primeWindows: "20260708a",
   primeRoundControlAudit: "20260708a",
-  primeDominanceLedgers: "20260708g-elite-stakes-subledger",
+  primeDominanceLedgers: "20260708h-elite-stakes-backfill",
   scoreWeighting: "20260708d-prime-dominance-data-restart-loader",
   championshipResumeLive: "20260708e",
   opponentQualityLive: "20260708b"
