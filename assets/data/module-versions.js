e9dcb4f6c928a930b9f0a1533ce6ae6d6c03ed1d
codyking0602 <bcking06@gmail.com>
2026-07-08 13:36:10 -0500

    Create module-versions.js

---
+++ assets/data/module-versions.js
@@ -0,0 +1,8 @@
+// Central cache-bust versions for scoring/category modules.
+window.UFC_MODULE_VERSIONS = {
+  primeWindows: "20260708a",
+  primeDominanceLedgers: "20260708b-data-restart",
+  scoreWeighting: "20260708d-prime-dominance-data-restart-loader",
+  championshipResumeLive: "20260708e",
+  opponentQualityLive: "20260708b"
+};
