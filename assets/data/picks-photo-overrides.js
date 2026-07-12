e13f9f465789c1ef680cebfb5ec00311241e27af
codyking0602 <bcking06@gmail.com>
2026-07-11 19:35:50 -0500

    Wire remaining UFC 329 fighter photos

--- assets/data/picks-photo-overrides.js
+++ assets/data/picks-photo-overrides.js
@@ -4,5 +4,10 @@ window.UFC_PICKS_PHOTOS = {
   "Benoit Saint Denis": "assets/fighters/benoit-saint-denis-thumb.webp",
   "Paddy Pimblett": "assets/fighters/paddy-pimblett-thumb.webp",
   "Cory Sandhagen": "assets/fighters/cory-sandhagen-thumb.webp",
-  "Mario Bautista": "assets/fighters/mario-bautista-thumb.webp"
+  "Mario Bautista": "assets/fighters/mario-bautista-thumb.webp",
+  "Brandon Royval": "assets/fighters/brandon-royval-thumb.webp",
+  "Lone'er Kavanagh": "assets/fighters/loneer-kavanagh-thumb.webp",
+  "King Green": "assets/fighters/king-green-thumb.webp",
+  "Terrance McKinney": "assets/fighters/terrance-mckinney-thumb.webp",
+  "Nikita Krylov": "assets/fighters/nikita-krylov-thumb.webp"
 };
