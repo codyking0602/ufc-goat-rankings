acceb8574e2000e66ea91ad81be2b3ac13b20c04
codyking0602 <bcking06@gmail.com>
2026-07-12 15:01:44 -0500

    Bust repaired Oklahoma City artwork cache

--- assets/data/picks-event-visuals.js
+++ assets/data/picks-event-visuals.js
@@ -1,6 +1,6 @@
 window.UFC_PICKS_EVENT_VISUALS = Object.assign({}, window.UFC_PICKS_EVENT_VISUALS || {}, {
   'ufc-oklahoma-city-2026-07-18': {
-    hero: 'assets/events/ufc-oklahoma-city-du-plessis-usman.webp',
+    hero: 'assets/events/ufc-oklahoma-city-du-plessis-usman.webp?v=20260712b-repaired',
     alt: 'Dricus Du Plessis vs. Kamaru Usman UFC Oklahoma City promotional artwork'
   }
 });
