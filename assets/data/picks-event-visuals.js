a7417a2ce5003051e1aaee0f33a0140d1f832691
codyking0602 <bcking06@gmail.com>
2026-07-12 14:45:52 -0500

    Map Oklahoma City event artwork

---
+++ assets/data/picks-event-visuals.js
@@ -0,0 +1,6 @@
+window.UFC_PICKS_EVENT_VISUALS = Object.assign({}, window.UFC_PICKS_EVENT_VISUALS || {}, {
+  'ufc-oklahoma-city-2026-07-18': {
+    hero: 'assets/events/ufc-oklahoma-city-du-plessis-usman.webp',
+    alt: 'Dricus Du Plessis vs. Kamaru Usman UFC Oklahoma City promotional artwork'
+  }
+});
